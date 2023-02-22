0f8d804c5c8ed43c6325e5b15f953c43b6224cb4
marcin10lw <marcin10lw@wp.pl>
2023-02-22 17:38:22 +0100

    added Pagination's illustrative logic

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   StyledPagination,
   ButtonsWrapper,
@@ -11,34 +12,60 @@ import {
 } from "./styled";
 
 const Pagination = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const onGoToFirst = () => {
+    if (currentPage !== 1) {
+      setCurrentPage(1);
+    }
+  };
+
+  const onGoToPrevious = () => {
+    if (currentPage !== 1) {
+      setCurrentPage((currentPage) => currentPage - 1);
+    }
+  };
+
+  const onGoToNext = () => {
+    if (currentPage !== 500) {
+      setCurrentPage((currentPage) => currentPage + 1);
+    }
+  };
+
+  const onGoToLast = () => {
+    if (currentPage !== 500) {
+      setCurrentPage(500);
+    }
+  };
+
   return (
     <StyledPagination>
       <ButtonsWrapper>
-        <Button>
-          <LeftArrow mobile />
+        <Button onClick={onGoToFirst}>
+          <LeftArrow mobile="true" />
           <LeftArrow />
           <ButtonText>First</ButtonText>
         </Button>
-        <Button>
+        <Button onClick={onGoToPrevious}>
           <LeftArrow />
           <ButtonText>Previous</ButtonText>
         </Button>
       </ButtonsWrapper>
       <PageInfo>
         <InfoText>Page</InfoText>
-        <InfoNumber>1</InfoNumber>
+        <InfoNumber>{currentPage}</InfoNumber>
         <InfoText>of</InfoText>
         <InfoNumber>500</InfoNumber>
       </PageInfo>
       <ButtonsWrapper>
-        <Button>
+        <Button onClick={onGoToNext}>
           <ButtonText>Next</ButtonText>
           <RightArrow />
         </Button>
-        <Button>
+        <Button onClick={onGoToLast}>
           <ButtonText>Last</ButtonText>
           <RightArrow />
-          <RightArrow mobile />
+          <RightArrow mobile="true" />
         </Button>
       </ButtonsWrapper>
     </StyledPagination>
