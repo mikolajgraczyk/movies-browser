86ea36fd4d9cd7927c9ba3a2d4c81397adff72b2
Jakub Helizanowicz <jakubhelizanowicz@gmail.com>
2023-02-22 15:21:17 +0100

    made a movie list ( mostly for Tile view )

---
+++ src/feautures/MovieList/index.js
@@ -0,0 +1,26 @@
+import { Tile } from "../../common/Tile";
+import { StyledMovies } from "./styled";
+
+const MovieList = () => {
+
+    return (
+        <StyledMovies>
+            <Tile
+                poster=""
+                title="Mulan: Forever and Ever"
+                subtitle="2023"
+                // rate=""
+                // votes=""
+            />
+            <Tile
+                poster=""
+                title="Mulan: Forever and Ever"
+                subtitle="2023"
+                // rate=""
+                // votes=""
+            />
+        </StyledMovies>
+    );
+};
+
+export default MovieList;
