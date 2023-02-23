a3011c5f1dbf71b7218c7b5f0d0a060bda117bed
marcin10lw <marcin10lw@wp.pl>
2023-02-23 18:49:14 +0100

    wrapped App components in HashRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Pagination from "./common/Pagination";
 import Header from "./common/Header";
+import ActorTile from "./features/actors/ActorTile";
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <HashRouter>
       <Header />
       <Pagination />
-    </>
+      <ActorTile />
+    </HashRouter>
   );
 }
 
