1f65858452fa671fc323bd689682e1c54e3285ca
marcin10lw <marcin10lw@wp.pl>
2023-02-23 18:49:42 +0100

    added whisper and silver colors

--- src/theme.js
+++ src/theme.js
@@ -7,6 +7,8 @@ export const theme = {
     mineShaft: "#333333",
     pattensBlue: "#D6E4FF",
     scienceBlue: "#0044CC",
+    whisper: "#F5F5FA",
+    silver: "#C4C4C4",
   },
   breakpoint: {
     mobile: 767,
