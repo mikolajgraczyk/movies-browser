2f0b0b106ef62867a167430880768332878315c0
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-02-23 17:47:38 +0100

    media queries added

--- src/features/ErrorPage/styled.js
+++ src/features/ErrorPage/styled.js
@@ -1,27 +1,44 @@
 import styled from "styled-components";
 import { ReactComponent as ErrorIcon } from "./ErrorIcon.svg";
 
+const bpMobile = ({ theme }) => theme.breakpoint.mobile;
+
 export const StyledErrorPage = styled.div`
     display: grid;
     justify-items: center;
-    gap: 36px;
+    gap: 38px;
     margin-top: 195px;
+
+    @media (max-width: ${bpMobile}px) {
+        gap: 14px;
+    }
 `;
 
 export const StyledErrorImage = styled(ErrorIcon)`
-
+    @media (max-width: ${bpMobile}px) {
+        width: 80px;
+    }
 `;
 
 export const Wrapper = styled.div`
     display: grid;
     justify-items: center;
     gap: 24px;
+
+    @media(max-width: ${bpMobile}px) {
+        gap: 16px;
+    }
 `;
 
 export const ErrorText = styled.div`
     font-size: 36px;
     font-weight: 600;
+    text-align: center;
     color: ${({ theme }) => theme.color.woodsmoke};
+
+    @media (max-width: ${bpMobile}px) {
+        font-size: 20px;
+    }
 `;
 
 export const ErrorDescription = styled.div`
@@ -29,7 +46,11 @@ export const ErrorDescription = styled.div`
     font-weight: 500;
     line-height: 130%;
     text-align: center;
-    width: 426px;
+    max-width: 426px;
+
+    @media (max-width: ${bpMobile}px) {
+        font-size: 16px;
+    }
 `;
 
 export const Button = styled.button`
@@ -40,10 +61,15 @@ export const Button = styled.button`
     line-height: 19px;
     border: none;
     border-radius: 5px;
-    color: ${({theme}) => theme.color.white};
+    color: ${({ theme }) => theme.color.white};
     background-color: ${({ theme }) => theme.color.scienceBlue};
 
     &:hover{
         cursor: pointer;
+    }
+
+    @media (max-width: ${bpMobile}px) {
+        font-size: 12px;
+        padding: 12px 16px;
     }
 `;
