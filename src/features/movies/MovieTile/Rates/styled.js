0b23f7fc360195b8eeae890f4e5043565940418f
marcin10lw <marcin10lw@wp.pl>
2023-02-25 08:16:38 +0100

    adjusted MovieTile's Rates and Tags stylings

--- src/features/movies/MovieTile/Rates/styled.js
+++ src/features/movies/MovieTile/Rates/styled.js
@@ -2,44 +2,41 @@ import styled from "styled-components";
 import { ReactComponent as Star } from "./star.svg";
 
 export const StyledRates = styled.div`
-    position: absolute;
-    bottom: 0;
+  position: absolute;
+  bottom: 0;
+  margin: 10px 0 0;
+  height: 24px;
+  display: flex;
+  align-items: stretch;
+  flex-wrap: wrap; // just in case there is a lot votes
+  gap: 12px;
+  font-size: 16px;
+  line-height: 150%;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    position: static;
+    gap: 7px;
+    font-size: 13px;
+    line-height: 130%;
     margin: 8px 0 0;
-    height: 24px;
-    display: flex;
-    gap: 12px;
-    align-items: stretch;
-    font-size: 16px;
-    line-height: 150%;
-
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-        position: static;
-        gap: 4px;
-        font-size: 13px;
-        line-height: 130%;
-    }
+  }
 `;
 
-export const Votes = styled.p`
-    margin: 0;
-    color: #7e839a;
-    font-weight: 400;
-
+export const Votes = styled.span`
+  color: ${({ theme }) => theme.color.waterloo};
 `;
 
-export const Score = styled.p`
-    margin: 0;
-    color: #18181b;
-    font-weight: 600;
-
+export const Score = styled.span`
+  color: ${({ theme }) => theme.color.woodsmoke};
+  font-weight: 600;
 `;
 
 export const StyledStar = styled(Star)`
-    width: 24px;
-    height: 21px;
+  width: 24px;
+  height: 21px;
 
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-        width: 16px;
-        height: 16px;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    width: 16px;
+    height: 16px;
+  }
 `;
--- src/features/movies/MovieTile/Tags/styled.js
+++ src/features/movies/MovieTile/Tags/styled.js
@@ -1,26 +1,24 @@
 import styled from "styled-components";
 
-
 export const StyledTags = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    gap: 8px;
-    margin: 0;
-    padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+  margin: 8px 0 0 0;
+  padding: 0;
 `;
 
 export const Tag = styled.span`
-    padding: 8px 16px;
-    background: #E4E6F0;
-    border-radius: 5px;
-    color: #18181B;
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 140%;
+  padding: 8px 16px;
+  background: #e4e6f0;
+  border-radius: 5px;
+  color: #18181b;
+  font-size: 14px;
+  line-height: 140%;
 
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-        padding: 4px 8px;
-        font-size: 10px;
-        line-height: 110%;
-    }     
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    padding: 4px 8px;
+    font-size: 10px;
+    line-height: 110%;
+  }
 `;
