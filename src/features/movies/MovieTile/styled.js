b4884cbd251937494bbe2920c08eace5e481ac58
marcin10lw <marcin10lw@wp.pl>
2023-02-25 08:16:11 +0100

    adjusted MovieTile styling

--- src/features/movies/MovieTile/styled.js
+++ src/features/movies/MovieTile/styled.js
@@ -1,17 +1,21 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import posterImage from "./images/video.svg";
 
-export const StyledMovieTile = styled.div`
+const bpMobile = ({ theme }) => theme.breakpoint.mobile;
+
+export const StyledMovieTile = styled(Link)`
+  text-decoration: none;
   display: grid;
   grid-template-columns: 1fr;
   grid-gap: 16px;
   padding: 16px;
-  background: #fff;
+  background-color: ${({ theme }) => theme.color.white};
   border-radius: 5px;
   box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
   height: 650px;
 
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+  @media (max-width: ${bpMobile}px) {
     grid-template-columns: auto 1fr;
     height: 201px;
   }
@@ -30,7 +34,7 @@ export const Poster = styled.img`
   border-radius: 5px;
   object-fit: cover;
 
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+  @media (max-width: ${bpMobile}px) {
     width: 114px;
     height: 169px;
   }
@@ -44,28 +48,29 @@ export const Title = styled.h3`
   font-size: 22px;
   line-height: 130%;
 
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+  @media (max-width: ${bpMobile}px) {
     font-size: 16px;
   }
 `;
 
 export const Subtitle = styled.p`
-  margin: 8px 0;
-  color: #7e839a;
+  margin: 8px 0 0 0;
+  color: ${({ theme }) => theme.color.waterloo};
   font-weight: 400;
   font-size: 16px;
   line-height: 150%;
 
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+  @media (max-width: ${bpMobile}px) {
     font-size: 13px;
     line-height: 130%;
+    margin: 4px 0 0 0;
   }
 `;
 
 export const Dummy = styled.div`
   width: 292px;
   height: 434px;
-  background-color: #c4c4c4;
+  background-color: ${({ theme }) => theme.color.silver};
   border-radius: 5px;
   object-fit: cover;
   background-image: url(${posterImage});
@@ -73,7 +78,7 @@ export const Dummy = styled.div`
   background-position: center;
   background-size: 28%;
 
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+  @media (max-width: ${bpMobile}px) {
     width: 114px;
     height: 169px;
     background-size: auto;
