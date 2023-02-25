1605389c819f202984ccc8be28b5f370575a0792
marcin10lw <marcin10lw@wp.pl>
2023-02-25 10:48:58 +0100

    implemented finding API Tags logic

--- src/features/movies/MovieTile/Tags/index.js
+++ src/features/movies/MovieTile/Tags/index.js
@@ -1,14 +1,31 @@
+import { useState, useEffect } from "react";
 import { StyledTags, Tag } from "./styled";
 
-const Tags = () => {
+const Tags = ({ genreIds }) => {
+  const base_url = "https://api.themoviedb.org/3";
+  const api_key = "api_key=0e017d1cf3aa44c76887c24c592892f0";
 
-    return (
-        <StyledTags>
-            <Tag>Action</Tag>
-            <Tag>Adventure</Tag>
-            <Tag>Drama</Tag>
-        </StyledTags>
-    );
+  const [genres, setGenres] = useState([]);
+
+  useEffect(() => {
+    fetch(`${base_url}/genre/movie/list?${api_key}&language=en-US`)
+      .then((response) => response.json())
+      .then(({ genres }) =>
+        setGenres(
+          genreIds.map((genreId) => {
+            return genres.find((genre) => genre.id === genreId);
+          })
+        )
+      );
+  }, []);
+
+  return (
+    <StyledTags>
+      {genres.map(({ id, name }) => (
+        <Tag key={id}>{name}</Tag>
+      ))}
+    </StyledTags>
+  );
 };
 
 export default Tags;
