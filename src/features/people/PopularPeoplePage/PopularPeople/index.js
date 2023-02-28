9518109a413afe1606c19e9554df9bca81062d9c
Jakub Helizanowicz <jakubhelizanowicz@gmail.com>
2023-02-28 18:33:01 +0100

    setup status page for popularPeople

---
+++ src/features/people/PopularPeoplePage/PopularPeople/index.js
@@ -0,0 +1,30 @@
+import { Container } from "../../../../common/Container";
+import TilesSection from "../../../../common/TilesSection";
+import ActorTile from "../../ActorTile/index";
+import { useSelector } from "react-redux";
+import { selectPeople } from "../../peopleSlice";
+import { GridList } from "../../../../common/GridList";
+
+const PopularPeople = () => {
+  const people = useSelector(selectPeople);
+
+  return (
+    <Container>
+      <TilesSection title="Popular people">
+        <GridList popularPeople>
+          {people &&
+            people.map(person => (
+              <li key={person.id}>
+                <ActorTile
+                  poster={person.profile_path}
+                  name={person.name}
+                />
+              </li>
+            ))}
+        </GridList>
+      </TilesSection>
+    </Container>
+  );
+};
+
+export default PopularPeople;
