84b26a6cde6e7a776c7e786dcb413bd70a419e19
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-03-01 19:45:13 +0100

    changed getGenreName function name to getGenres

--- src/features/getData.js
+++ src/features/getData.js
@@ -11,7 +11,7 @@ export const getPopularData = async (dataName, page = 1) => {
   return data.results;
 };
 
-export const getGenreName = async () => {
+export const getGenres = async () => {
   const { data } = await axios.get(
     `${baseUrl}/genre/movie/list?${apiKey}&language=en-US`
   );
