bdeac2ae942c3a0f05b7c94ec6ee53cd10b2a62e
marcin10lw <marcin10lw@wp.pl>
2023-03-02 16:00:19 +0100

    added new colors to theme

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,7 @@
 export const theme = {
   color: {
     white: "#FFFFFF",
+    black: "000000",
     mystic: "#E4E6F0",
     waterloo: "#7E839A",
     woodsmoke: "#18181B",
@@ -9,6 +10,7 @@ export const theme = {
     scienceBlue: "#0044CC",
     whisper: "#F5F5FA",
     silver: "#C4C4C4",
+    stormGrey: "#74788B",
   },
   breakpoint: {
     mobile: 767,
