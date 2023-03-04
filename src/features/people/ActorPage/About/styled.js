9d817de876b38f54d75ab0283edf122ef98a3a73
marcin10lw <marcin10lw@wp.pl>
2023-03-04 19:47:09 +0100

    styled About with grid-template-areas

--- src/features/people/ActorPage/About/styled.js
+++ src/features/people/ActorPage/About/styled.js
@@ -1,15 +1,32 @@
 import styled from "styled-components";
 
+const bpMobile = ({ theme }) => theme.breakpoint.mobile;
+
 export const StyledAbout = styled.section`
   display: grid;
   grid-template-columns: minmax(116px, 399px) auto;
+  grid-template-areas:
+    "i h"
+    "i d";
   padding: 40px;
-  gap: 40px;
+  column-gap: 40px;
+
+  @media (max-width: ${bpMobile}px) {
+    column-gap: normal;
+    gap: 16px;
+    padding: 16px;
+  }
 `;
 
 export const ActorImage = styled.img`
+  display: block;
   width: 100%;
   border-radius: 5px;
+  grid-area: 1 / 1 / 3 / 2;
+
+  @media (max-width: ${bpMobile}px) {
+    grid-row: 1 / 2;
+  }
 `;
 
 export const ActorInfo = styled.div``;
@@ -18,6 +35,11 @@ export const Name = styled.h1`
   font-size: 36px;
   font-weight: 600;
   margin: 0 0 24px;
+
+  @media (max-width: ${bpMobile}px) {
+    font-size: 14px;
+    margin: 0 0 16px;
+  }
 `;
 
 export const BirthInfo = styled.div`
@@ -26,6 +48,12 @@ export const BirthInfo = styled.div`
   gap: 8px;
   margin: 0 0 24px;
   font-size: 18px;
+  grid-area: d;
+
+  @media (max-width: ${bpMobile}px) {
+    font-size: 12px;
+    margin: 0 0 4px;
+  }
 `;
 
 export const SpecificInfo = styled.span`
@@ -37,4 +65,10 @@ export const ActorBio = styled.p`
   margin: 0;
   font-size: 20px;
   line-height: 1.6;
+  grid-area: d;
+
+  @media (max-width: ${bpMobile}px) {
+    grid-column: 1 / 3;
+    font-size: 14px;
+  }
 `;
