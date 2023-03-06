fc1bec8ccea7cb7c34a3451ccc2a9ca4551caa49
Jakub Helizanowicz <jakubhelizanowicz@gmail.com>
2023-03-06 20:50:29 +0100

    update queryParams to be shown in NoResults

--- src/common/NoResultsPage/index.js
+++ src/common/NoResultsPage/index.js
@@ -3,11 +3,16 @@ import { Container } from "../Container";
 import {
   StyledNoResultsImage,
 } from "./styled";
+import { useSearchParams } from "react-router-dom";
 
 const NoResultsPage = () => {
+    const searchQueryParamName = "search";
+    const [searchParams] = useSearchParams({ [searchQueryParamName]: "" });
+    const query = searchParams.get(searchQueryParamName);
+
   return (
     <Container>
-      <TilesSection title="Sorry, there are no results for “ ”">
+      <TilesSection title={`Sorry there are no results for “${query}”`} >
         <StyledNoResultsImage />
       </TilesSection>
     </Container>
