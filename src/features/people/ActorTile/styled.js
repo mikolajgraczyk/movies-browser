5cf882bb2d328f7fe39a1aa31c6ecfde45a0a8d0
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-03-06 09:51:49 +0100

    other approach to solve the problem with same results

--- src/features/people/ActorTile/styled.js
+++ src/features/people/ActorTile/styled.js
@@ -11,6 +11,7 @@ export const StyledActorTile = styled(Link)`
   text-decoration: none;
   background-color: ${({ theme }) => theme.color.white};
   transition: all 170ms cubic-bezier(0.45, 0.05, 0.55, 0.95);
+  height: 100%;
 
   &:hover {
     transform: translateY(-4px);
@@ -41,12 +42,10 @@ export const ActorName = styled.h3`
   font-weight: 500;
   margin: 12px 0 0 0;
   color: ${({ theme }) => theme.color.woodsmoke};
-  height: 64px;
 
   @media (max-width: ${bpMobile}px) {
     font-size: 14px;
     margin: 8px 0 0 0;
-    height: 36px;
   }
 `;
 
