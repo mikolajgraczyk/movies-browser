e4a051234fe0edebaa9ec3d24388c5695bd55a21
marcin10lw <marcin10lw@wp.pl>
2023-03-07 18:47:39 +0100

    gave ActorPage error handling

--- src/features/people/ActorPage/actorSaga.js
+++ src/features/people/ActorPage/actorSaga.js
@@ -1,13 +1,20 @@
 import { call, delay, put, takeLatest } from "redux-saga/effects";
 import { getPersonData } from "../../getData";
-import { fetchActorDetails, fetchActorDetailsSuccess } from "./actorSlice";
+import {
+  fetchActorDetails,
+  fetchActorDetailsFail,
+  fetchActorDetailsSuccess,
+} from "./actorSlice";
 
 function* fetchActorDetailsHandler({ payload: actorId }) {
   try {
     const actorData = yield call(getPersonData, actorId);
     yield delay(500);
     yield put(fetchActorDetailsSuccess(actorData));
-  } catch (error) {}
+  } catch (error) {
+    yield delay(500);
+    yield put(fetchActorDetailsFail());
+  }
 }
 
 export function* actorSaga() {
